8bd0277bf233049421dc3e000be7056b2e4c756b
Dmitriy Grigoriev <prowebsup@gmail.com>
2018-04-21 06:48:15 +0700

    woody
    js

---
+++ woodymoody/js/common.js
@@ -0,0 +1,10 @@
+$(function() {
+
+	$(".navbar-nav a").mPageScroll2id();
+	$(".footer-menu a").mPageScroll2id();
+
+	/*--- Start PopUp ---*/
+	$(".fancybox").fancybox();
+	/*--- End Popup ---*/
+
+});
