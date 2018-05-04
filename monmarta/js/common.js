728836b87540ea5ee4e8b34833c55e378722d04d
Dmitriy Grigoriev <prowebsup@gmail.com>
2018-05-04 18:16:42 +0700

    new5

--- monmarta/js/common.js
+++ monmarta/js/common.js
@@ -3,4 +3,27 @@ $(function() {
 	$(".navbar-nav a").mPageScroll2id();
 
 
+	/* Scroll To Top start */
+
+	$(document).ready(function() {
+		$(window).scroll(function() {
+        // Высота проявления кнопки
+        if ($(this).scrollTop() > 100) {
+        	$('#go-to-top').fadeIn();
+        } else {
+        	$('#go-to-top').fadeOut();
+        }
+      });  
+		$('#go-to-top').click(function() {
+			$('body,html').animate({
+				scrollTop: 0
+        // Скорость подъема
+      }, 1000);
+			return false;
+		});
+	});
+
+	/* Scroll To Top end */
+
+
 });
