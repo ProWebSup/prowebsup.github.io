8adeccfa92a421e664a160743e91b9ff87ed9027
Dmitriy Grigoriev <prowebsup@gmail.com>
2018-10-30 01:07:29 +0700

    Js and Sass
    Js and Sass

---
+++ manticorre/js/common.js
@@ -0,0 +1,38 @@
+$(function() {
+
+	// $(".navbar-nav a").mPageScroll2id();
+	// $(".footer-menu a").mPageScroll2id();
+
+	/*--- Start PopUp ---*/
+	// $(".fancybox").fancybox();
+	/*--- End Popup ---*/
+
+
+	// $("#date").mask("99/99/9999",{placeholder:"mm/dd/yyyy"});
+	// $("#client-phone").mask("+7 (999) 999-99-99");
+
+
+
+		/* Scroll To Top start */
+
+	// $(document).ready(function() {
+	// 	$(window).scroll(function() {
+ //        // Высота проявления кнопки
+ //        if ($(this).scrollTop() > 100) {
+ //        	$('#go-to-top').fadeIn();
+ //        } else {
+ //        	$('#go-to-top').fadeOut();
+ //        }
+ //      });  
+	// 	$('#go-to-top').click(function() {
+	// 		$('body,html').animate({
+	// 			scrollTop: 0
+ //        // Скорость подъема
+ //      }, 1000);
+	// 		return false;
+	// 	});
+	// });
+
+	/* Scroll To Top end */
+
+});
