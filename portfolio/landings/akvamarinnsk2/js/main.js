d40e806d7263c5947737823484d6623901d4ef63
Dmitriy Grigoriev <37729809+ProWebSup@users.noreply.github.com>
2020-02-26 18:36:32 +0700

    Update
    Update lanading

--- portfolio/landings/akvamarinnsk2/js/main.js
+++ portfolio/landings/akvamarinnsk2/js/main.js
@@ -11,12 +11,12 @@ $(document).ready(function() {
 	}, { passive: true });
 
 
-$(".fancybox").fancybox();
+	$(".fancybox").fancybox();
 
 
-  $(".eTimer").eTimer({
-    etType: 0, etDate: "19.06.2020.0.0", etTitleText: "До открытия гостиницы осталось:", etTitleSize: 20, etShowSign: 1, etSep: ":", etFontFamily: "Trebuchet MS", etTextColor: "#a3a3a3", etPaddingTB: 15, etPaddingLR: 15, etBackground: "white", etBorderSize: 0, etBorderRadius: 2, etBorderColor: "white", etShadow: " 0px 0px 10px 0px white", etLastUnit: 4, etNumberFontFamily: "Impact", etNumberSize: 35, etNumberColor: "white", etNumberPaddingTB: 0, etNumberPaddingLR: 8, etNumberBackground: "#ffcc33", etNumberBorderSize: 0, etNumberBorderRadius: 5, etNumberBorderColor: "white", etNumberShadow: "inset 0px 0px 10px 0px rgba(0, 0, 0, 0.5)"
-  });
+	$(".eTimer").eTimer({
+		etType: 0, etDate: "19.06.2020.0.0", etTitleText: "До открытия гостиницы осталось:", etTitleSize: 20, etShowSign: 1, etSep: ":", etFontFamily: "Trebuchet MS", etTextColor: "#a3a3a3", etPaddingTB: 15, etPaddingLR: 15, etBackground: "white", etBorderSize: 0, etBorderRadius: 2, etBorderColor: "white", etShadow: " 0px 0px 10px 0px white", etLastUnit: 4, etNumberFontFamily: "Impact", etNumberSize: 35, etNumberColor: "white", etNumberPaddingTB: 0, etNumberPaddingLR: 8, etNumberBackground: "#ffcc33", etNumberBorderSize: 0, etNumberBorderRadius: 5, etNumberBorderColor: "white", etNumberShadow: "inset 0px 0px 10px 0px rgba(0, 0, 0, 0.5)"
+	});
 
 
 	/*scroll to anchor*/
@@ -41,13 +41,14 @@ $(".fancybox").fancybox();
   $("input,select,textarea").not("[type=submit]").jqBootstrapValidation();
   
 
-  $("#modal-phone").mask("+7 (999) 999-99-99");
+  $("#header-phone").mask("+7 (999) 999-99-99");
+  $("#action-phone").mask("+7 (999) 999-99-99");
    // $("#actionPhonel").mask("+7 (999) 999-99-99");
   // $("#yourtelabout").mask("+7 (999) 999-99-99");
 
 
 
-	/*requisites*/
+  /*requisites*/
 	// $('.footer__requisites').append('<p>ООО «СибСваи»</p><p>ИНН 5410065473</p><p>ОГРН 1175476019025</p>');
 
 
