931f09806bd671d02479e70c6f745c62b8e027ee
Dmitriy Grigoriev <37729809+ProWebSup@users.noreply.github.com>
2020-08-21 19:07:53 +0700

    akvamarinnsk
    upload

---
+++ portfolio/akvamarinnsk/form_action.js
@@ -0,0 +1,106 @@
+$(function() {
+
+    "use strict";
+
+
+
+var form = $("#request_action"),
+        successMessage = "Ваше письмо успешно отправлено!", // success message
+        warningMessage = "Вы заполнили не все поля!", // warning message
+        errorMessage = "Что-то пошло не так. Попробуйте позже!";  // error message
+
+
+        form.on("submit", function(event) {
+            event.preventDefault();
+
+            $.ajax({
+                url: "form.php",
+                type: "POST",
+                data: form.serialize(),
+                success: function(response) {
+                    var data = JSON.parse(response),
+                    message;
+
+                    if(data.status == 'success') {
+                        message = successMessage;
+                    }
+
+                    if(data.status == 'warning') {
+                        message = warningMessage;
+                    }
+
+                    if(data.status == 'error') {
+                        message = errorMessage;
+                    }
+
+                    callAlert(message, data.status);
+                },
+                error: function(response) {
+                    callAlert(errorMessage, "error");
+                }
+            });
+        });
+
+
+
+
+
+
+    /**
+    *** Show alert
+    **/
+
+    var alertTimeout,
+    delay = 500000000; // Delay to alert fade out
+
+    function removeAlert() {
+        clearTimeout(alertTimeout);
+        alertTimeout = setTimeout(function() {
+            $(".alert").stop().fadeOut(function() {
+                $(this).remove();
+            });
+        }, delay);
+    }
+
+    function callAlert(message, type) {
+        $(".alert").stop().remove();
+
+        var alertClass;
+
+        if(type == "success") {
+            alertClass = "success";
+        }
+        if(type == "error") {
+            alertClass = "error";
+        }
+        if(type == "warning") {
+            alertClass = "warning";
+        }
+
+        // var alert = '<div class="alert  alert--shadow  alert--'+ alertClass + '"><button class="alert__button" type="button"><i class="fa fa-times" aria-hidden="true"></i></button>&nbsp;' + message + '</div>';
+
+
+        var alert = '<div class="alert alert-success alert-dismissible shadow-lg" role="alert"><strong>' + message + '</strong><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'
+
+
+        $("body").append(alert);
+
+        removeAlert();
+    }
+
+
+    $(function() {
+        $("body").on("click", ".alert__button", function(event) {
+            event.preventDefault();
+
+            var $this = $(this),
+            alert = $this.parents(".alert");
+
+            alert.fadeOut("fast", function() {
+                $(this).remove();
+                clearTimeout(alertTimeout);
+            });
+        });
+    });
+
+});
---
+++ portfolio/akvamarinnsk/js/main.js
@@ -0,0 +1,58 @@
+
+$(document).ready(function() {
+	/*navbar*/
+	document.addEventListener("scroll", function (e) {
+		if (window.pageYOffset > 150) {
+			if ($(window).width() >= 990)
+				{ 	  $('.nav').addClass('nav--show'); }
+		} else {
+			$('.nav').removeClass('nav--show');
+		}
+	}, { passive: true });
+
+
+	$(".fancybox").fancybox();
+
+
+	$(".eTimer").eTimer({
+		etType: 0, etDate: "19.06.2020.0.0", etTitleText: "До открытия гостиницы осталось:", etTitleSize: 20, etShowSign: 1, etSep: ":", etFontFamily: "Trebuchet MS", etTextColor: "#a3a3a3", etPaddingTB: 15, etPaddingLR: 15, etBackground: "white", etBorderSize: 0, etBorderRadius: 2, etBorderColor: "white", etShadow: " 0px 0px 10px 0px white", etLastUnit: 4, etNumberFontFamily: "Impact", etNumberSize: 35, etNumberColor: "white", etNumberPaddingTB: 0, etNumberPaddingLR: 8, etNumberBackground: "#ffcc33", etNumberBorderSize: 0, etNumberBorderRadius: 5, etNumberBorderColor: "white", etNumberShadow: "inset 0px 0px 10px 0px rgba(0, 0, 0, 0.5)"
+	});
+
+
+	/*scroll to anchor*/
+	$('.scrollTo').on('click', function (event) {
+		event.preventDefault();
+
+		$('html, body').animate({
+			scrollTop: $($.attr(this, 'href')).offset().top - 40
+		}, 500);
+	});
+
+	/*scroll to top*/
+	$('.nav__logo').on('click', function () {
+		$("html, body").animate({ scrollTop: 0 }, 500);
+		return false;
+	});
+
+
+
+
+  // $(".modal .modal-body .form-group input, select, textarea").jqBootstrapValidation();
+  $("input,select,textarea").not("[type=submit]").jqBootstrapValidation();
+  
+  $("#modal-phone").mask("+7 (999) 999-99-99");
+  $("#header-phone").mask("+7 (999) 999-99-99");
+  $("#action-phone").mask("+7 (999) 999-99-99");
+   // $("#actionPhonel").mask("+7 (999) 999-99-99");
+  // $("#yourtelabout").mask("+7 (999) 999-99-99");
+
+
+
+  /*requisites*/
+	// $('.footer__requisites').append('<p>ООО «СибСваи»</p><p>ИНН 5410065473</p><p>ОГРН 1175476019025</p>');
+
+
+
+
+
+});
